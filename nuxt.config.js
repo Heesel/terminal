8981a32e5c44fdfb419991d2bb251b7c73b535c5
Heesel <hesselpa@live.nl>
2022-12-30 17:48:04 +0100

    moved scrollbar css to public to hopefully resolve 404

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,5 @@
 import { defineNuxtConfig } from 'nuxt'
 
-// https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
     buildModules: ['@nuxtjs/tailwindcss'],
     app: {
@@ -12,7 +11,7 @@ export default defineNuxtConfig({
           ],
           link: [
             { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,400;1,300;1,400&display=swap' },
-            { rel: 'stylesheet', href: './assets/css/scrollbar.css' }
+            { rel: 'stylesheet', href: './public/scrollbar.css' }
           ],
         }
       },
